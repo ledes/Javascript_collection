55dfffd464b3bbbb6cc0fbdffd0817984158c5bd
Pablo <pablo.ledesma.p@gmail.com>
2016-01-12 16:10:41 -0500

    Leap year

--- Leap/leapYear.js
+++ Leap/leapYear.js
@@ -1,2 +1,5 @@
-// Write a program that will take a year and report if it is a leap year.
-// 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+function leapYear(year){
+  return  (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)) ;
+};
+
+console.log(leapYear(1990));
