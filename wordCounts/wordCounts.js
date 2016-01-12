0c85459776bef4d36ce85394d3ab07ab868668e1
Pablo <pablo.ledesma.p@gmail.com>
2016-01-12 16:11:21 -0500

    Word counts

--- wordCounts/wordCounts.js
+++ wordCounts/wordCounts.js
@@ -1,1 +1,22 @@
-// Write a program that given a phrase can count the occurrences of each word in that phrase.
+function WordCount(str) {
+    var R =  /(\w|\s)*\w(?=")|\w+/g;
+    var strArray = str.toLowerCase().match(R);
+    var strObject = {};
+    var finalStr = "";
+
+    for (var i = 0; i < strArray.length; i++){
+      word = strArray[i]
+      if (strObject[word]){
+        strObject[word] =  strObject[word] += 1 ;
+      } else {
+          strObject[word] = 1;
+      };
+    };
+
+    for (var key in strObject){
+      finalStr += key + ": " + strObject[key] + "\n";
+    }
+    return finalStr
+}
+
+console.log(WordCount("Hello hello world, hello WoRLD"))
