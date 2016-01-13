89b10c6554dad7f8ea945406a1a0c93f316c8106
Pablo <pablo.ledesma.p@gmail.com>
2016-01-12 19:30:34 -0500

    Number exercise done

--- phoneNumber/phoneNumber.js
+++ phoneNumber/phoneNumber.js
@@ -1,7 +1,23 @@
-// The rules are as follows:
-//
-// If the phone number is less than 10 digits assume that it is bad number
-// If the phone number is 10 digits assume that it is good
-// If the phone number is 11 digits and the first number is 1, trim the 1 and use the last 10 digits
-// If the phone number is 11 digits and the first number is not 1, then it is a bad number
-// If the phone number is more than 11 digits assume that it is a bad number
+function numberExtractor(phoneNumber) {
+  var badNumber = "Bad number"
+  var phoneNumberStr= phoneNumber.toString()
+
+  if (phoneNumberStr.length == 10) {
+    return phoneNumber;
+  } else {
+    if (phoneNumberStr.length == 11) {
+      if (phoneNumberStr[0] == "1") {
+        return phoneNumberStr.substr(1);
+      }else{
+        return badNumber;
+      };
+    } else {
+      return badNumber;
+    };
+  };
+};
+
+console.log(numberExtractor(1234567890)); // returns 1234567890
+console.log(numberExtractor(12234567890)); //returns 2234567890
+console.log(numberExtractor(567890)); //returns  bad number
+console.log(numberExtractor(567890567890)); // returns bad number
